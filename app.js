1fc887b6611a41fc74c110e7da3b8c9efb8f72a7
Erfan Paslar <paslarerfan1@gmail.com>
2022-04-05 02:19:25 +0430

    :/

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-if("serviceWorker" in navigator){
-  window.addEventListener("load", function () {
-    navigator.serviceWorker.getRegistrations().then((r)=>{r[0].unregister()})
-
-    navigator.serviceWorker.register("/Mini-Blackjack/serviceWorker.js").then(function(registration){
-      console.log("Service Worker Registered");
-    }).catch(function(err){
-      console.log("Service Worker Not Register", err);
-    });
-  })
-}
+// if("serviceWorker" in navigator){
+//   window.addEventListener("load", function () {
+//     navigator.serviceWorker.getRegistrations();
+//     navigator.serviceWorker.register("/serviceWorker.js").then(function(registration){
+//       console.log("Service Worker Registered");
+//     }).catch(function(err){
+//       console.log("Service Worker Not Register", err);
+//     });
+//   })
+// }
