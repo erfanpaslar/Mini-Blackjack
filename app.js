d2f74c655ef13361a71a7459d058016419d97fea
Erfan Paslar <paslarerfan1@gmail.com>
2022-04-05 02:16:41 +0430

    service worker

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if("serviceWorker" in navigator){
   window.addEventListener("load", function () {
     navigator.serviceWorker.getRegistrations().then((r)=>{r[0].unregister()})
 
-    navigator.serviceWorker.register("/serviceWorker.js").then(function(registration){
+    navigator.serviceWorker.register("serviceWorker.js").then(function(registration){
       console.log("Service Worker Registered");
     }).catch(function(err){
       console.log("Service Worker Not Register", err);
